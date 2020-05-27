0bc341c87414ff46cf1248ded3d0904dac3fc4be
tanilserbes <serbestanil@gmail.com>
2020-05-27 18:09:02 +0100

    updated

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,16 +1,16 @@
-// // Note Spec JS
-// // Reference test
+// Note Spec JS
+// Reference test
 // Writing out first tests
-//
-// function testCircleRadiusDefaultsTo10() {
-//   var circle = new Circle();
-//   assert.isTrue(circle.radius === 10);
-// };
-//
-// function testtest() {
-//   assert.toBe(10, 11);
-// };
-//
-//
-// testCircleRadiusDefaultsTo10();
-// testtest();
+
+function testCircleRadiusDefaultsTo10() {
+  var circle = new Circle();
+  assert.isTrue(circle.radius === 10);
+};
+
+function testtest() {
+  assert.toBe(10, 11);
+};
+
+
+testCircleRadiusDefaultsTo10();
+testtest();
