9652ba09de5aca358bf43aec4ab1e6e0c964d69e
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-29 17:32:56 +0100

    Updates testing framework

--- spec/assert.js
+++ spec/assert.js
@@ -1,19 +1,55 @@
-var assert = {
+"use strict";
 
-  isTrue: function(assertionToCheck) {
-    if (!assertionToCheck) {
-      throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
-    } else {
-      console.log("Pass");
-    }
-  },
+function expect(actual){
 
-  toEq: function(first, second) {
-    if (first !== second) {
-      throw new Error("Assertion failed: got " + first + " expected " + second);
-    } else {
-      console.log("Pass");
+  return {
+    toEqual: function(expected){
+      if(actual === expected){
+        console.log("Pass")
+      } else {
+        console.log("Fail")
+      }
+    },
+
+    toBeAnArray: function(){
+      if(actual.constructor.name === "Array"){
+        console.log("Pass")
+      } else {
+        console.log("Fail")
+      }
     }
   }
+}
 
-};
+function it(label, callback){
+  console.log(`Test: ${label}`)
+  callback()
+}
+
+function describe(label, callback){
+  console.log(`- ${label} -`)
+  callback()
+}
+
+
+
+
+// var assert = {
+//
+//   isTrue: function(assertionToCheck) {
+//     if (!assertionToCheck) {
+//       throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+//     } else {
+//       console.log("Pass");
+//     }
+//   },
+//
+//   toEq: function(first, second) {
+//     if (first !== second) {
+//       throw new Error("Assertion failed: got " + first + " expected " + second);
+//     } else {
+//       console.log("Pass");
+//     }
+//   }
+//
+// };
--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,37 +1,75 @@
-function testAllNotesArray() {
-  var note = new Note();
-  assert.toEq(note.allNotes.length,(0));
-};
+describe('allNotes', function() {
+  it('array starts off empty', function(){
+    var note = new Note();
+    expect(note.allNotes.length).toEqual(0)
+  });
+});
 
-testAllNotesArray();
+describe('#createNote', function() {
+  it('Adds a note to the allNotes array', function(){
+    var note = new Note;
+    note.createNote("Test");
+    expect(note.allNotes[0]).toEqual("Test");
+  });
+});
 
-function testCreateNoteAddsNote() {
-  var note = new Note;
-  note.createNote("Test");
-  assert.toEq(note.allNotes[0], "Test");
-};
+describe('#abbreviateNote', function() {
+  it('return the first 20 characters of one input note', function(){
+    var note = new Note;
+    note.createNote("My name is Andrew and I love Makers Academy, most of the time")
+    note.abbreviateNote();
 
-testCreateNoteAddsNote();
+    expect(note.allAbbreviateNotes[0]).toEqual("My name is Andrew an...");
+  });
+
+  it('returns the first 20 characters of multiple input notes', function(){
+    var note = new Note;
+    note.createNote("My name is Andrew and I love Makers Academy, most of the time")
+    note.createNote("My name is Tanil1 and I love Makers Academy, most of the time")
+    note.abbreviateNote();
+
+    expect(note.allAbbreviateNotes[0]).toEqual("My name is Andrew an...")
+    expect(note.allAbbreviateNotes[1]).toEqual("My name is Tanil1 an...")
+  });
+
+});
 
 
-function testAbbreviatedNotes() {
-  var note = new Note;
-  note.createNote("My name is Andrew and I love Makers Academy, most of the time")
-  note.abbreviateNote();
-  assert.toEq(note.allAbbreviateNotes[0], "My name is Andrew an...")
-};
-
-testAbbreviatedNotes();
 
 
-function testAbbreviatedNotes2() {
-  var note = new Note;
-  note.createNote("My name is Andrew and I love Makers Academy, most of the time")
-  note.createNote("My name is Tanil1 and I love Makers Academy, most of the time")
-  note.abbreviateNote();
-  assert.toEq(note.allAbbreviateNotes[0], "My name is Andrew an...")
-  console.log('the next one')
-  assert.toEq(note.allAbbreviateNotes[1], "My name is Tanil1 an...")
-};
 
-testAbbreviatedNotes2();
+// function testAllNotesArray() {
+//   var note = new Note();
+//   assert.toEq(note.allNotes.length,(0));
+// };
+//
+// testAllNotesArray();
+
+// function testCreateNoteAddsNote() {
+//   var note = new Note;
+//   note.createNote("Test");
+//   assert.toEq(note.allNotes[0], "Test");
+// };
+//
+// testCreateNoteAddsNote();
+
+// function testAbbreviatedNotes() {
+//   var note = new Note;
+//   note.createNote("My name is Andrew and I love Makers Academy, most of the time")
+//   note.abbreviateNote();
+//   assert.toEq(note.allAbbreviateNotes[0], "My name is Andrew an...")
+// };
+//
+// testAbbreviatedNotes();
+
+// function testAbbreviatedNotes2() {
+//   var note = new Note;
+//   note.createNote("My name is Andrew and I love Makers Academy, most of the time")
+//   note.createNote("My name is Tanil1 and I love Makers Academy, most of the time")
+//   note.abbreviateNote();
+//   assert.toEq(note.allAbbreviateNotes[0], "My name is Andrew an...")
+//   console.log('the next one')
+//   assert.toEq(note.allAbbreviateNotes[1], "My name is Tanil1 an...")
+// };
+//
+// testAbbreviatedNotes2();
